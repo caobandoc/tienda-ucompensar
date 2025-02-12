8278faeff438e096d76e4471c1a4e4506a4bbfef
caobandoc <obandocarlosandres@gmail.com>
2025-02-11 20:08:12 -0500

    add new parameters

--- src/app/feature/crm/pages/crm/crm.component.ts
+++ src/app/feature/crm/pages/crm/crm.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import {ToolbarComponent} from '../../components/toolbar/toolbar.component';
 import {TableComponent} from '../../components/table/table.component';
+import {ProductService} from '../../../../core/services/product.service';
+import {Product} from '../../../../core/models/product';
 
 @Component({
   selector: 'app-crm',
@@ -12,5 +14,14 @@ import {TableComponent} from '../../components/table/table.component';
   styleUrl: './crm.component.scss'
 })
 export class CrmComponent {
+  products : Product[] = [];
+
+  constructor(
+    private readonly productService: ProductService
+  ) {
+    this.productService.getProducts().subscribe((products) => {
+      this.products = products;
+    });
+  }
 
 }
