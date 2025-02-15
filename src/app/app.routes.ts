9781fd26f4422632afd44ec2fde4e0a0b12f90bc
caobandoc <obandocarlosandres@gmail.com>
2025-02-15 15:50:02 -0500

    update crm

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './feature/home/pages/home/home.component';
 import {ListproductsComponent} from './feature/home/pages/listproducts/listproducts.component';
-import {crmRoutes} from './feature/crm/pages/crm/crm.routes';
+import {LoginComponent} from './feature/home/pages/login/login.component';
+import {RegisterComponent} from './feature/home/pages/register/register.component';
+import {CrmComponent} from './feature/crm/pages/crm/crm.component';
+import {ProductsComponent} from './feature/crm/pages/products/products.component';
 
 export const routes: Routes = [{
   path: '',
@@ -10,6 +13,16 @@ export const routes: Routes = [{
   path: 'list',
   component: ListproductsComponent
 }, {
+  path: 'login',
+  component: LoginComponent
+}, {
+  path: 'register',
+  component: RegisterComponent
+}, {
   path: 'crm',
-  children: crmRoutes
+  component: CrmComponent,
+  children: [{
+    path: 'products',
+    component: ProductsComponent
+  }]
 }];
--- src/app/feature/crm/pages/crm/crm.component.ts
+++ src/app/feature/crm/pages/crm/crm.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-crm',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    RouterLink
   ],
   templateUrl: './crm.component.html',
   styleUrl: './crm.component.scss'
