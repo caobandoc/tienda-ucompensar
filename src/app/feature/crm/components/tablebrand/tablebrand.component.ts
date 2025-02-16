019e84de56a6cd0716730a0b661dd183b5fdccfa
Joifel <armando.ospina@gmail.com>
2025-02-15 22:05:47 -0500

    Se realiza la página de Marcas

--- src/app/feature/crm/components/tablebrand/tablebrand.component.ts
+++ src/app/feature/crm/components/tablebrand/tablebrand.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Brand } from '../../../../core/models/Brand';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tablebrand',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './tablebrand.component.html',
   styleUrl: './tablebrand.component.scss'
 })
 export class TablebrandComponent {
-  @Input() displayedColumns: string[] = [];
-  @Input() dataSource: Brand[] = [];
+  @Input() displayedColumns: string[] = ['id', 'nombre', 'acciones'];
+  @Input() dataSource: Brand[] = [  
+  ];
 }
--- src/app/feature/crm/pages/brands/brands.component.ts
+++ src/app/feature/crm/pages/brands/brands.component.ts
@@ -9,7 +9,16 @@ import { Brand } from '../../../../core/models/Brand';
   styleUrl: './brands.component.scss'
 })
 export class BrandsComponent {
-  brands : Brand[] = [{id:1, name:'Carlos'}, {id:2, name:'Camilo'}];
+  brands : Brand[] = [{ id: 1, name: 'Apple' },
+    { id: 2, name: 'Samsung' },
+    { id: 3, name: 'Sony' },
+    { id: 4, name: 'LG' },
+    { id: 5, name: 'Dell' },
+    { id: 6, name: 'HP' },
+    { id: 7, name: 'Lenovo' },
+    { id: 8, name: 'Asus' },
+    { id: 9, name: 'Acer' },
+    { id: 10, name: 'Microsoft' }];
   //datos quemados de columnas
   displayedColumns: string[] = ['id', 'nombre', 'acciones'];
 }
